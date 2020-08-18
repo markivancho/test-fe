71b36ab18ccc2685fbe413467a3b00a304da7495
Mark <markiyan@pindify.com>
2020-08-18 13:47:51 +0300

    Updates

--- precache-manifest.e6a9443950d63bd3fe9eac608bd7b421.js
+++ precache-manifest.e6a9443950d63bd3fe9eac608bd7b421.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1a1b6054806d0461462622703b661a3e",
+    "revision": "a8862507f72ea914021014dc7bce3c35",
     "url": "/test-fe/index.html"
   },
   {
-    "revision": "7b24ad0ca55b85ca4d67",
+    "revision": "9015aed82a8698fda30f",
     "url": "/test-fe/static/css/main.56aadfd3.chunk.css"
   },
   {
-    "revision": "735baab7b6b90d7a97db",
-    "url": "/test-fe/static/js/2.de8d1cdd.chunk.js"
+    "revision": "1376e19dff0389193c21",
+    "url": "/test-fe/static/js/2.96728f97.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/test-fe/static/js/2.de8d1cdd.chunk.js.LICENSE.txt"
+    "url": "/test-fe/static/js/2.96728f97.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7b24ad0ca55b85ca4d67",
-    "url": "/test-fe/static/js/main.ce00e6ec.chunk.js"
+    "revision": "9015aed82a8698fda30f",
+    "url": "/test-fe/static/js/main.3331198a.chunk.js"
   },
   {
     "revision": "425ec2a20834973412e8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-fe/precache-manifest.47877fec87423ccd36362aa1bd5fe109.js"
+  "/test-fe/precache-manifest.e6a9443950d63bd3fe9eac608bd7b421.js"
 );
 
 self.addEventListener('message', (event) => {
