f681dff8342322237af5361819c9d165277edee0
Mark <markiyan@pindify.com>
2020-08-19 19:23:18 +0300

    add darker color

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -7,6 +7,7 @@ const ImagePlaceholder = () => (
     width={500}
     height={400}
     backgroundColor="#f2f2f2"
+    foregroundColor="#ececec"
     viewBox="0 0 500 400"
   >
     <rect x="2" y="2" rx="0" ry="0" width="500" height="400" />
