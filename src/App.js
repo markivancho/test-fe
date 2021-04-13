0d5a68a3c2c1284e257a35716496018c81cc069e
Markiyan Ivancho <mark.i@matic.com>
2021-04-13 12:34:07 +0300

    push changes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-import Header from './Header'
-import Start from './screens/Start'
-import Result from './screens/Result'
-import Questions from './screens/Questions'
+import Header from "./Header";
+import Start from "./screens/Start";
+import Result from "./screens/Result";
+import Questions from "./screens/Questions";
 
-import DataContext from './utils/DataContext'
+import DataContext from "./utils/DataContext";
 
 function App() {
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
 
   return (
     <BrowserRouter>
       <DataContext.Provider value={[data, setData]}>
         <Header />
         <Switch>
-          <Route exact path="/test-fe">
+          <Route exact path="/">
             <Start />
           </Route>
-          <Route path="/test-fe/questions">
+          <Route path="/questions">
             <Questions />
           </Route>
-          <Route path="/test-fe/result">
+          <Route path="/result">
             <Result />
           </Route>
           <Route>
-            <Redirect to="/test-fe" />
+            <Redirect to="/" />
           </Route>
         </Switch>
       </DataContext.Provider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
