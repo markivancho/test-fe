75eadf16f773ebfaf95cec562a4e07ffdfa08c0a
Markiyan Ivancho <mark.i@matic.com>
2021-04-13 13:02:41 +0300

    trigger failing build

--- src/screens/Result/__tests__/Result.spec.jsx
+++ src/screens/Result/__tests__/Result.spec.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import { shallow } from 'enzyme'
+import React from "react";
+import { shallow } from "enzyme";
 
-import Result from '../index'
+import Result from "../index";
 
 let mockContext = [
-  { first_name: 'Cool', last_name: 'Guy', phone: '(111) 333-4444' },
-]
+  { first_name: "Cool", last_name: "Guy", phone: "(111) 333-4444" },
+];
 
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
   useContext: () => mockContext,
-}))
+}));
 
-it('uses data from context', () => {
-  const component = shallow(<Result />)
+it("uses data from context", () => {
+  const component = shallow(<Result />);
 
-  expect(component.find('Heading').props().children).toEqual(
-    'Thanks for Your Answers, Cool Guy. We Will Call You at (111) 333-4444 Within 24 Hours'
-  )
-})
+  expect(component.find("Heading").props().children).toEqual(
+    "Thanks for Your Answers, Cool Guy. We Will Call You at (111) 333-4444 Within 24 Hours"
+  );
+});
 
-it('redirects if context is empty', () => {
-  mockContext = [{}]
-  const component = shallow(<Result />)
+it("redirects if context is empty", () => {
+  mockContext = [{}];
+  const component = shallow(<Result />);
 
-  expect(component.find('Redirect').props().to).toEqual('/test-fe')
-})
+  expect(component.find("Redirect").props().to).toEqual("/test-fe");
+});
